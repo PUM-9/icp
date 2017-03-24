eef55851f8b0180a3cd4291b2f45f693d6fd56c0
Ubuntu i rummet <nej@nej>
2017-03-24 09:30:45 +0100

    code for pasting clouds

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <pcl/point_types.h>
+#include <pcl/common/transforms.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/io/vtk_io.h>
 #include <pcl/io/vtk_lib_io.h>
@@ -11,60 +12,71 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/surface/poisson.h>
 
-int main (int argc, char** argv)
-{
+int main(int argc, char **argv) {
     std::cout << "Loading files" << std::endl;
 
-    for (int i = 0; i < 10; i++) {
+    //for (int i = 0; i < 10; i++) {
 
-        // Load input file into a PointCloud<T> with an appropriate type
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    // Load input file into a PointCloud<T> with an appropriate type
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-        std::stringstream ss;
-        if(i < 10) {
-            ss << "cube000" << i << ".pcd";
-        } else {
-            ss << "cube00" << i << ".pcd";
-        }
+    std::stringstream ss;
+    /*if(i < 10) {
+        ss << "cube000" << i << ".pcd";
+    } else {
+        ss << "cube00" << i << ".pcd";
+    }*/
+    ss << "halfcube0.pcd";
 
-        pcl::PCLPointCloud2 cloud_blob;
+    pcl::PCLPointCloud2 cloud_blob;
 
-        if (pcl::io::loadPCDFile (ss.str(), cloud_blob) == -1) {
-            std::cout << "Couldn't read file " << ss.str() << std::endl;
-        }
-
-        pcl::fromPCLPointCloud2 (cloud_blob, *cloud);
-
-        // The data should be available in the *cloud
-
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_first(new pcl::PointCloud<pcl::PointXYZ>);
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_second(new pcl::PointCloud<pcl::PointXYZ>);
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-
-        // Create the filtering object
-        pcl::PassThrough<pcl::PointXYZ> pass;
-        pass.setInputCloud (cloud);
-        pass.setFilterFieldName ("x");
-        pass.setFilterLimits (0.0, 1.0);
-        pass.setFilterLimitsNegative (true);
-        pass.filter (*cloud_filtered_first);
-
-        pass.setInputCloud(cloud_filtered_first);
-        pass.setFilterFieldName("z");
-        pass.setFilterLimits(-10, 10);
-        pass.setFilterLimitsNegative(true);
-        pass.filter(*cloud_filtered_second);
-
-        // Remove points that are far away from other points
-        pcl::RadiusOutlierRemoval<pcl::PointXYZ> outlier_filter;
-        outlier_filter.setInputCloud(cloud_filtered_second);
-        outlier_filter.setRadiusSearch(0.8);
-        outlier_filter.setMinNeighborsInRadius(2);
-        outlier_filter.filter(*cloud_filtered);
-
-        std::cout << "Saving filtered cloud" << std::endl;
-        pcl::io::savePCDFile(ss.str(), *cloud_filtered);
+    if (pcl::io::loadPCDFile(ss.str(), cloud_blob) == -1) {
+        std::cout << "Couldn't read file " << ss.str() << std::endl;
     }
+
+    pcl::fromPCLPointCloud2(cloud_blob, *cloud);
+
+    // The data should be available in the *cloud
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_first(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_second(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_third(new pcl::PointCloud<pcl::PointXYZ>);
+
+
+    // Create the filtering object
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud(cloud);
+    pass.setFilterFieldName("x");
+    pass.setFilterLimits(400, 513);
+    pass.setFilterLimitsNegative(false);
+    pass.filter(*cloud_filtered_first);
+
+    /*pass.setInputCloud(cloud_filtered_first);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(-300, 0);
+    pass.setFilterLimitsNegative(true);
+    pass.filter(*cloud_filtered_second);*/
+
+    pass.setInputCloud(cloud_filtered_first);
+    pass.setFilterFieldName("y");
+    pass.setFilterLimits(-1, 1);
+    pass.setFilterLimitsNegative(true);
+    pass.filter(*cloud_filtered_third);
+
+    // Remove points that are far away from oneanother points
+    pcl::RadiusOutlierRemoval<pcl::PointXYZ> outlier_filter;
+    outlier_filter.setInputCloud(cloud_filtered_third);
+    outlier_filter.setRadiusSearch(0.8);
+    outlier_filter.setMinNeighborsInRadius(2);
+    outlier_filter.filter(*cloud_filtered);
+
+
+
+
+    std::cout << "Saving filtered cloud" << std::endl;
+    pcl::io::savePCDFile("halfcube0filtered.pcd", *cloud_filtered);
+    // }
 
     return (0);
 }
--- src/icptest.cpp
+++ src/icptest.cpp
@@ -6,73 +6,76 @@
 #include <pcl/registration/icp.h>
 
 int
- main (int argc, char** argv)
-{
+main(int argc, char **argv) {
 
     // for (int i = 0; i < 9; i++) {
 
-        pcl::PointCloud<pcl::PointXYZI>::Ptr cloudIn (new pcl::PointCloud<pcl::PointXYZI>);
-        pcl::PointCloud<pcl::PointXYZI>::Ptr cloudOut (new pcl::PointCloud<pcl::PointXYZI>);
-        pcl::PointCloud<pcl::PointXYZI> finalCloud;
-        pcl::PointCloud<pcl::PointXYZI>::Ptr cloudOut_new (new pcl::PointCloud<pcl::PointXYZI>) ;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloudIn(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloudOut(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PointCloud<pcl::PointXYZI> finalCloud;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloudOut_new(new pcl::PointCloud<pcl::PointXYZI>);
 
-        //if(i == 0) {
-            if(pcl::io::loadPCDFile (argv[1], *cloudIn)==-1)
-            {
-                std::cout << argv[0] << std::endl;
-                PCL_ERROR ("Couldn't read first file! \n");
-                return (-1);
-            }
-        //} else {
-            //if(pcl::io::loadPCDFile ("cuberesult.pcd", *cloudIn)==-1)
-            //{
-               // PCL_ERROR ("Couldn't read result file! \n");
-              //  return (-1);
-           // }
-        //}
+    //if(i == 0) {
+    if (pcl::io::loadPCDFile("halfcube100filtered.pcd", *cloudIn) == -1) {
+        std::cout << argv[0] << std::endl;
+        PCL_ERROR ("Couldn't read first file! \n");
+        return (-1);
+    }
+    //} else {
+    //if(pcl::io::loadPCDFile ("cuberesult.pcd", *cloudIn)==-1)
+    //{
+    // PCL_ERROR ("Couldn't read result file! \n");
+    //  return (-1);
+    // }
+    //}
 
-        //save aligned pair, transformed into the first cloud's frame
-        //std::stringstream ss;
-        //ss << "cube000" << i + 1 << ".pcd";
+    //save aligned pair, transformed into the first cloud's frame
+    //std::stringstream ss;
+    //ss << "cube000" << i + 1 << ".pcd";
 
-        //std::cout << ss.str() << std::endl;
-        if(pcl::io::loadPCDFile (argv[2], *cloudOut)==-1)
-        {
-            PCL_ERROR ("Couldn't read second input file! \n");
-            return (-1);
-        }
+    //std::cout << ss.str() << std::endl;
+    if (pcl::io::loadPCDFile("cuberesult.pcd", *cloudOut) == -1) {
+        PCL_ERROR ("Couldn't read second input file! \n");
+        return (-1);
+    }
+    Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
 
-        pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp;
+    float theta = M_PI/2; // The angle of rotation in radians
+    transform_2.rotate(Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitY()));
+    // Executing the transformation
+    pcl::PointCloud<pcl::PointXYZI>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZI>());
+    // You can either apply transform_1 or transform_2; they are the same
+    pcl::transformPointCloud(*cloudOut, *transformed_cloud, transform_2);
 
-        // Parameters for the ICP algorithm
+    pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp;
 
-        icp.setInputSource(cloudOut);
-        icp.setInputTarget(cloudIn);
-        icp.setMaximumIterations(25);
-        icp.setTransformationEpsilon(1e-8);
-        icp.setMaxCorrespondenceDistance(0.05);
-        icp.setEuclideanFitnessEpsilon(1);
-        icp.setRANSACOutlierRejectionThreshold(1);
+    // Parameters for the ICP algorithm
 
-        icp.align(*cloudOut);
+    icp.setInputSource(cloudOut);
+    icp.setInputTarget(cloudIn);
+    icp.setMaximumIterations(25);
+    icp.setTransformationEpsilon(1e-5);
+    icp.setMaxCorrespondenceDistance(90);
+    //icp.setEuclideanFitnessEpsilon(1);
+    //icp.setRANSACOutlierRejectionThreshold(1);
 
-        if (icp.hasConverged())
-        {
-            std::cout << "ICP converged." << std::endl
-                      << "The score is " << icp.getFitnessScore() << std::endl;
-            std::cout << "Transformation matrix:" << std::endl;
-            std::cout << icp.getFinalTransformation() << std::endl;
-        }
-        else std::cout << "ICP did not converge." << std::endl;
+    icp.align(*cloudOut);
 
-        Eigen::Matrix4f transformationMatrix = icp.getFinalTransformation ();
-        std::cout<<"trans %n"<<transformationMatrix<<std::endl;
+    if (icp.hasConverged()) {
+        std::cout << "ICP converged." << std::endl
+                  << "The score is " << icp.getFitnessScore() << std::endl;
+        std::cout << "Transformation matrix:" << std::endl;
+        std::cout << icp.getFinalTransformation() << std::endl;
+    } else std::cout << "ICP did not converge." << std::endl;
 
-        pcl::transformPointCloud( *cloudOut, *cloudOut_new, transformationMatrix);
+    Eigen::Matrix4f transformationMatrix = icp.getFinalTransformation();
+    std::cout << "trans %n" << transformationMatrix << std::endl;
 
-        finalCloud = *cloudIn + *cloudOut;
+    pcl::transformPointCloud(*cloudOut, *cloudOut_new, transformationMatrix);
 
-        pcl::io::savePCDFileASCII ("cuberesult.pcd", finalCloud);
+    finalCloud = *cloudIn + *cloudOut;
+
+    pcl::io::savePCDFileASCII("cuberesult2.pcd", finalCloud);
     //}
 
     return (0);
